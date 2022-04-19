58cf46ee87f217ad6749dad72f6e3c0edc41b628
DaimanWebb <dwebb019@ucr.edu>
2022-04-19 15:41:13 -0700

    I deleted the files I made to keep clean and consistent
    e enter the commit message for your changes. Lines starting

--- simple.cpp
+++
@@ -1,8 +0,0 @@
-int main() {
-    int *p = new int[10];
-    p[9] = 1; //fixed size
-
-    delete[] p; //Deallocate memory (incorrectly)
-
-    return 0;
-}
--- uninitialized.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    int x = 420;
-    cout << x << endl;
-
-    return 0;
-}
