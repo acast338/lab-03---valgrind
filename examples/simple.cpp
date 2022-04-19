7bf670052ee35a7e7f293dec07ec6dd1a3cc50d8
acast338 <acast338@ucr.edu>
2022-04-18 20:51:59 -0700

    Fix deallocation to correct notation

--- examples/simple.cpp
+++ examples/simple.cpp
@@ -2,7 +2,7 @@ int main() {
     int *p = new int[10];
     p[9] = 1;
 
-    delete p;   // Deallocate the memory from the new above
+    delete[] p;   // Deallocate the memory from the new above
 
     return 0;
 }
